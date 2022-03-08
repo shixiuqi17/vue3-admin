1fe9ad1a4922e1bb26ad7de83f766afdad7c3a56
Hans <shixiuqimail@163.com>
2022-03-08 09:08:31 +0800

    fix: utils内函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 // 以/分割的字符串定义成驼峰
 export function toHumpName(str: string) {
-  var re = /-(\w)/g; // \w匹配所有的字母和数字字符以及下划线_
-  return str.replace(re, function ($0, $1) {
+  let reg = /-(\w)/g; // \w匹配所有的字母和数字字符以及下划线_
+  return str.replace(reg, function ($0, $1) {
     // 第一个参数re表示正则,第二个回调函数,对该字符串处理方式,圆括号内的形参数表示正则表达式的子选项
     return $1.toUpperCase(); // 转大写
   });
