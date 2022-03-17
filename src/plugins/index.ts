f7b77f1f60636f10250d119bd3a9d3eed869e956
Hans <shixiuqimail@163.com>
2022-03-17 15:41:40 +0800

    feat: 安装配置pinia插件

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,9 +1,11 @@
 import { App } from "vue";
 import setupElementPlus from "./elementPlus";
 import setupI18n from "./i18n";
+import setupPinia from "./pinia";
 
 // 插件引入
 export function setupPlugins(app: App) {
   setupElementPlus(app);
   setupI18n(app);
+  setupPinia(app);
 }
