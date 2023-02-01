ff9167d29390c8677c20d7250afca810081418dd
shixiuqi <shixiuqimail@163.com>
2023-02-01 15:22:06 +0800

    fix: 本地存储有值，验证格式是否符合要求

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,21 +1,33 @@
 // 以/分割的字符串定义成驼
 export function toHumpName(str: string) {
-  let reg = /-(\w)/g; // \w匹配所有的字母和数字字符以及下划线_
+  let reg = /-(\w)/g // \w匹配所有的字母和数字字符以及下划线_
   return str.replace(reg, function ($0, $1) {
     // 第一个参数re表示正则,第二个回调函数,对该字符串处理方式,圆括号内的形参数表示正则表达式的子选项
-    return $1.toUpperCase(); // 转大写
-  });
+    return $1.toUpperCase() // 转大写
+  })
 }
 
 // 拼接跳转路由路径
 export function handleRouteLink(routePath: string, routeChildPath: string) {
-  if (routePath === "" || /^\//.test(routeChildPath)) {
-    return routeChildPath;
+  if (routePath === '' || /^\//.test(routeChildPath)) {
+    return routeChildPath
   }
 
-  if (routeChildPath === "") {
-    return routePath;
+  if (routeChildPath === '') {
+    return routePath
   }
 
-  return `${routePath}/${routeChildPath}`;
+  return `${routePath}/${routeChildPath}`
+}
+
+// 判断是否为JSON格式
+export function isJSON(val: string) {
+  if (typeof val == 'string') {
+    try {
+      let obj = JSON.parse(val)
+      return obj && typeof obj == 'object'
+    } catch (e) {
+      return false
+    }
+  }
 }
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,29 +1,31 @@
+import { isJSON } from './index'
+
 export interface StoreData {
-  expire?: number;
-  [key: string]: any;
+  expire?: number
+  [key: string]: any
 }
 
 const store = {
   set(key: string, data: StoreData): void {
     // 设置过期时间;
     if (data.expire) {
-      data.expire = new Date().getTime() + data.expire * 100;
+      data.expire = new Date().getTime() + data.expire * 100
     }
-    localStorage.setItem(key, JSON.stringify(data));
+    localStorage.setItem(key, JSON.stringify(data))
   },
   get(key: string): StoreData | null {
-    const item = localStorage.getItem(key);
-    if (item) {
-      const data = JSON.parse(item);
-      // 判断是否过期
-      if (data.expire && data.expire < new Date().getTime()) {
-        localStorage.removeItem(key);
-        return null;
-      }
-      return data;
-    }
-    return null;
-  }
-};
+    const item = localStorage.getItem(key)
 
-export default store;
+    if (item === 'undefined' || !item) return null
+    
+    const data = isJSON(item) ? JSON.parse(item) : item
+    // 判断是否过期
+    if (data.expire && data.expire < new Date().getTime()) {
+      localStorage.removeItem(key)
+      return null
+    }
+    return data
+  }
+}
+
+export default store
